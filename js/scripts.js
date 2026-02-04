a74ebb4840789e1ce4147b939dd135b93fbd6b2a
MarineG404 <marine.gonnord.pro@gmail.com>
2026-02-04 13:06:52 +0100

    Refactor resource loading to handle API errors and improve local storage management

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
+async function loadRessources(){
+
+    let localStorageData = localStorage.getItem("ressources");
+
+    if (!localStorageData){
+        try {
+            const apiResponse = await fetch('./data/data.json');
+           
+            if (!apiResponse.ok) {
+                if (apiResponse.status === 404) {
+                    throw new Error("Erreur 404 : Le fichier JSON est introuvable.");
+                } else if (apiResponse.status === 500) {
+                    throw new Error("Erreur 500 : Le serveur a un problème technique.");
+                } else {
+                    throw new Error("Erreur HTTP : " + apiResponse.status);
+                }
+            };
+
+            const dataAPI = await apiResponse.json();
+            console.log("Données chargées :", dataAPI);
+
+            localStorage.setItem("ressources", JSON.stringify(dataAPI));
+
+        } catch (error) {
+            console.error("Erreur capturée :", error.message);   
+        }
+    }
+    
+    displayRessources();
+}
+
 function displayRessources(){
     const allArticles = JSON.parse(localStorage.getItem("ressources"));
     const divArticles = document.getElementById("articles");
@@ -14,4 +45,4 @@ function displayRessources(){
     });
 }
 
-displayRessources();
+loadRessources();
